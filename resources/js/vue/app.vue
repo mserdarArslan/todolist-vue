c391e1d9c008331b6575f78fe8433b6f0713f78f
mserdarArslan <79723397+mserdarArslan@users.noreply.github.com>
2021-05-14 23:32:41 +0300

    Basic appearance of the app completed.

--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -1,10 +1,37 @@
 <template>
-  <div>Hello Vue World...!</div>
+  <div class="todoListContainer">
+    <div class="heading">
+      <h2 id="title">ToDo List</h2>
+      <add-item-form />
+    </div>
+    <list-view />
+  </div>
 </template>
 
 <script>
-export default {};
+import addItemForm from "./addItemForm";
+import listView from "./listView";
+
+export default {
+  components: {
+    addItemForm,
+    listView,
+  },
+};
 </script>
 
-<style>
+<style scoped>
+.todoListContainer {
+  width: 350px;
+  margin: auto;
+}
+
+.heading {
+  background: #e6e6e6;
+  padding: 10px;
+}
+
+#title {
+  text-align: center;
+}
 </style>
