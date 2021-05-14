9990edba96efae86cbb8be9ff6f67f24efb271bc
mserdarArslan <79723397+mserdarArslan@users.noreply.github.com>
2021-05-15 00:18:21 +0300

    Some fonts installed and addItemForm completed. Now addItemForm component is working.

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,14 @@ import Vue from 'vue'
 //  - Now we import our vue.app file
 import App from './vue/app.vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPlusSquare, faTrash)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 // Here we create the Vue front-end application object.
 const app = new Vue({
     // create an element with an id of #app
