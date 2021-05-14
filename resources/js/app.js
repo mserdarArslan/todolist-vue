de183d567d25cd75e5441d64b9f49f825b3d37ac
mserdarArslan <79723397+mserdarArslan@users.noreply.github.com>
2021-05-14 23:05:01 +0300

    Basic Vue frontend integration completed.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 require('./bootstrap');
+
+//	- First import the vue package.
+import Vue from 'vue'
+//  - Now we import our vue.app file
+import App from './vue/app.vue'
+
+// Here we create the Vue front-end application object.
+const app = new Vue({
+    // create an element with an id of #app
+    el: '#app',
+    // pass in our app component that we just created
+    components: { App }
+});
