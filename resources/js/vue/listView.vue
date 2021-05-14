6a5505957749d7c1b36296da893bbb7cc6b47034
mserdarArslan <79723397+mserdarArslan@users.noreply.github.com>
2021-05-15 00:46:05 +0300

    ListView Componet is updated to include the listItems.

--- resources/js/vue/listView.vue
+++ resources/js/vue/listView.vue
@@ -1,7 +1,26 @@
-		<template>
-  <div>List view</div>
+<template>
+  <div>
+    <div v-for="(item, index) in items" :key="index">
+      <list-item :item="item" class="item" />
+    </div>
+  </div>
 </template>
 
-		<script>
-export default {};
+<script>
+import ListItem from "./listItem.vue";
+
+export default {
+  props: ["items"],
+  components: {
+    ListItem,
+  },
+};
 </script>
+
+<style scoped>
+.item {
+  background: #e6e6e6;
+  padding: 5px;
+  margin-top: 5px;
+}
+</style>
